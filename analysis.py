f692f85275a2d09751b14ab7a53ac744da18a993
Chu Lee <chu.lee14@imperial.ac.uk>
2015-03-21 23:37:50 +0000

    New analysis file

--- analysis.py
+++ analysis.py
@@ -7,6 +7,7 @@ import time
 import random
 
 g_users = {}
+g_used = {}
 
 def combine(self_id):
     data1 = pickle.load(open(self_id + "_data_save1.p", "rb"))
@@ -32,13 +33,25 @@ def load_data(data_file):
         c_users = [(k, v) for (k, v) in data[i][2].iteritems()]
         for (j_id, j_name) in c_users:
             users[j_id] = j_name.encode('utf8')
-            if not j_id in adj_list:
-                adj_list[j_id] = {}
+            # if not j_id in adj_list:
+            #     adj_list[j_id] = {}
+            # for (k_id, k_name) in c_users:
+            #     if j_id != k_id:
+            #         adj_list[j_id][k_id] = 0
+
+    for k in users.keys():
+        adj_list[k] = {}
+        for k2 in users.keys():
+            if k != k2:
+                adj_list[k][k2] = 0
+
+    for i in range(0, data_limit):
+        c_users = [(k, v) for (k, v) in data[i][2].iteritems()]
+        for (j_id, j_name) in c_users:
             for (k_id, k_name) in c_users:
                 if j_id != k_id:
-                    if not k_id in adj_list[j_id]:
-                        adj_list[j_id][k_id] = 0
                     adj_list[j_id][k_id] += 1
+                    adj_list[k_id][j_id] += 1
 
     for u_id in users.keys():
         tmp = sorted(adj_list[u_id].items(), key=lambda x: -x[1])
@@ -47,8 +60,8 @@ def load_data(data_file):
             tmp_dict[k] = v
         adj_list[u_id] = tmp_dict
         verbose = [(users[k], v) for (k, v) in tmp]
-        if u_id == self_id:
-            print users[u_id], verbose
+        # if u_id == self_id:
+        #     print users[u_id], verbose
         # print
 
     return adj_list, users
@@ -68,7 +81,7 @@ def build_clique(clique, threshold, starting_threshold, users, adj_list, used):
     used[cur_id] = 1
     friends = sorted(adj_list[cur_id].items(), key=lambda x: -x[1])
 
-    new_threshold = threshold * 0.3
+    new_threshold = threshold * 0.2
     if new_threshold < starting_threshold:
         new_threshold = starting_threshold
     if new_threshold < 3:
@@ -76,7 +89,8 @@ def build_clique(clique, threshold, starting_threshold, users, adj_list, used):
 
     for k, v in friends:
         if k not in clique and k not in used:
-            if v >= new_threshold:
+            if v >= new_threshold and v < threshold:
+                # print users[k], v, threshold
                 clique.append(k)
                 clique, used = build_clique(clique, v, starting_threshold, users, adj_list, used)
 
@@ -92,16 +106,51 @@ def analyse(self_id, users, adj_list):
     # Runs through all friends in decreasing order, make 1 call
     for (k, v) in my_friends:
         if k not in used:
-            clique, build_used = build_clique([self_id, k], v, v*0.1, users, adj_list, used)
+            clique, build_used = build_clique([self_id, k], v, v * 0.1, users, adj_list, used)
             if len(clique) > 3:
                 print verbose_list(clique)
             for (k, v) in build_used.iteritems():
                 used[k] = v
+
+def analyse2(self_id, min_threshold, adj_list):
+    global g_used
+    my_friends = sorted(adj_list[self_id].items(), key=lambda x: -x[1])
+    used = {}
+
+    for (k, v) in my_friends:
+        if k not in g_used:
+            clique, build_used = build_clique2(self_id, min_threshold, adj_list, g_used)
+            if len(clique) > 3:
+                print verbose_list(clique)
+            g_used.update(build_used)
+
+def build_clique2(self_id, min_threshold, adj_list, used):
+    my_friends = sorted(adj_list[self_id].items(), key=lambda x: -x[1])
+    clique = [self_id]
+
+    # Try to add people into the clique (in desc order)
+    for (k, v) in my_friends:
+        cur_id = k
+        add = True
+        for user in clique:
+            link_str = adj_list[user][cur_id]
+            if link_str < min_threshold:
+                add = False
+        if add:
+            if cur_id not in used.keys():
+                used[cur_id] = 1
+                clique.append(cur_id)
+
+    # print verbose_list(clique)
+    return clique, used
+
 
 if __name__ == '__main__':
     self_id = "601910818"
     # combine(self_id)
     adj_list, users = load_data("mark_data.p")
     g_users = users
-    analyse(self_id, users, adj_list)
+    # analyse(self_id, users, adj_list)
+    for i in range(40, 0, -2):
+        analyse2(self_id, i, adj_list)
     # print adj_list
